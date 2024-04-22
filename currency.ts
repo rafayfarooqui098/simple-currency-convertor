e1f1badcf0c4599cda52019260acf60088db9983
rafay farooqui <160497705+rafayfarooqui098@users.noreply.github.com>
2024-04-22 13:51:33 +0500

    currency convertor

---
+++ currency.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+const currency : any= {
+    USD : 1,
+    EUR : 0.91,
+    GBP : 0.76,
+    INR : 74.57,
+    PKR : 280,
+    
+} 
+
+let user_Ans = await inquirer.prompt([
+    {
+    name:"from",
+    message:"enter from currency",
+    type:"list",
+    choices:["USD","EUR","GBP","INR","PKR"]
+    },
+    {
+    name:"to",
+    message:"enter to currency",
+    type:"list",
+    choices:["USD","EUR","GBP","INR","PKR"]
+    },
+    {
+    name:"amount",
+    message:"enter your amount",
+    type:"number"
+    }
+]);
+
+let fromAmonut = currency[user_Ans.from]
+let toAmount = currency[user_Ans.to]
+let amount = user_Ans.amount
+let baseAmount = amount / fromAmonut//USD base currency
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount)
